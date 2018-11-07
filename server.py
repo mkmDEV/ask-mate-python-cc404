e2a72a43bdefead204e968760e84492819eea709
Kristóf Mátyás Marsi <mkmdev18@gmail.com>
2018-11-07 10:57:58 +0100

    Created route display_question, created single_question.html

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-
 import data_handler
 
 app = Flask(__name__)
@@ -10,12 +9,18 @@ app = Flask(__name__)
 def route_list():
     questions = data_handler.get_all_question(convert_linebreaks=True)
 
-    return render_template('list.html', questions=questions)
+    return render_template('list.html', questions=questions, page_title='Welcome')
+
+
+@app.route('question/<question_id')
+def display_question(question_id: int)
+    return (render_template("single_question.html")
 
 
 @app.route('/newquestion')
 def route_question():
     return render_template('question.html')
+
 
 if __name__ == '__main__':
     app.run(
