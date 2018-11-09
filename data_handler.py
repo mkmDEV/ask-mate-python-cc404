86824e82b24169ea6929124751e38658146ec11b
gaborTachtler <tachtler@gmail.com>
2018-11-09 02:43:33 +0100

    iplement add_new_question and add_new_answer option

--- data_handler.py
+++ data_handler.py
@@ -1,12 +1,12 @@
 import csv
 import datetime
+import calendar
+import time
+import os
 from operator import itemgetter
 
-QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-ANSWER_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
-
-def get_csv_data(file='question.csv', id=None, key=None, isquestions=True):
+def get_csv_data(file='data/question.csv', id=None, key=None, isquestions=True):
     all_data = []
     with open(file, encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file)
@@ -30,43 +30,70 @@ def get_timeform_from_stamp(table, istable=True):
         table['submission_time'] = datetime.datetime.fromtimestamp(int(table['submission_time'])).strftime('%Y-%m-%d %H:%M:%S')
     return table
 
+
+def get_unix_time():
+    return str(calendar.timegm(time.gmtime()))
+
+
 def sorter(table, keyvalue, isreverse=True):
     return sorted(table, key=itemgetter(keyvalue), reverse=isreverse)
 
 
-def get_next_id():
-    existing_data = get_all_question()
-
+def get_next_id(file):
+    existing_data = get_csv_data(file)
     if len(existing_data) == 0:
         return '1'
-
     return str(int(existing_data[-1]['id']) + 1)
 
 
-def add_user_story(story):
-    story['id'] = get_next_id()
-    story['status'] = DEFAULT_STATUS
+def add_message_to_file(request, file='data/question.csv', q_id='0'):
+    if file == 'data/question.csv':
+        fields = [get_next_id('data/question.csv'), get_unix_time(), '0', '0', request['title'], request['message']]
+    else:
+        fields = [get_next_id('data/answer.csv'), get_unix_time(), '0', q_id, request['message']]
+    with open(file, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
 
-    add_user_story_to_file(story, True)
+
+def create_updated_file(id):
+    with open('data/question.csv', 'r') as f:
+        with open('data/cache.csv', 'w') as f1:
+            for line in f:
+                if line[0] != id:
+                    f1.write(line)
 
 
-def update_user_story(story):
-    add_user_story_to_file(story, False)
+def update_original_file():
+    with open('data/cache.csv', 'r') as f:
+        with open('data/question.csv', 'w') as f1:
+            for line in f:
+                f1.write(line)
+    os.remove('data/cache.csv')
 
 
-def add_user_story_to_file(story, append=True):
-    existing_data = get_all_question()
+'''def create_updated_file2(id):
+    with open('data/answer.csv', 'r') as f:
+        with open('data/cache.csv', 'w') as f1:
+            for line in f:
+                print(line)
+                if line[3] == "3":
+                    print(line)
 
-    with open(QUESTIONS_FILE_PATH, 'w', newline='', encoding='utf-8') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=QUESTION_HEADER)
-        writer.writeheader()
 
-        for row in existing_data:
-            if not append:
-                if row['id'] == story['id']:
-                    row = story
+def update_original_file2():
+    with open('data/cache.csv', 'r') as f:
+        with open('data/answer.csv', 'w') as f1:
+            for line in f:
+                f1.write(line)
+    #os.remove('data/cache.csv')
 
-            writer.writerow(row)
 
-        if append:
-            writer.writerow(story)
+def update():
+    with open('data/answer.csv', 'rb') as inp, open('data/cache.csv', 'wb') as out:
+        writer = csv.writer(out)
+        for row in csv.reader(inp):
+            if row[3] != "3":
+                writer.writerow(row)
+
+update()'''
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 import data_handler
 
 app = Flask(__name__)
@@ -15,11 +15,11 @@ def home():
                            page_title='Welcome to AskMate!')
 
 
-@app.route('/question/<question_id>', methods=['GET', 'POST'])
+@app.route('/question/<question_id>')
 def display_question(question_id: int):
     question_data = data_handler.get_csv_data(id=question_id, key='id')
     question_data = data_handler.get_timeform_from_stamp(question_data, False)
-    answers = data_handler.get_csv_data('answer.csv', question_id, 'question_id', False)
+    answers = data_handler.get_csv_data('data/answer.csv', question_id, 'question_id', False)
     answers = data_handler.sorter(answers, 'submission_time')
     answers = data_handler.get_timeform_from_stamp(answers)
     return render_template('single_question.html',
@@ -31,27 +31,39 @@ def display_question(question_id: int):
 
 @app.route('/new-question')
 def write_new_question():
-    return render_template('question.html')
+    return render_template('new_question.html')
 
 
 @app.route('/new-question', methods=['POST'])
 def post_new_question():
+    data_handler.add_message_to_file(request.form)
+    return redirect('/')
+
+
+@app.route('/question/<question_id>/delete')
+def delete_question(question_id: int):
+    data_handler.create_updated_file(question_id)
+    data_handler.update_original_file()
+    #data_handler.create_updated_file2(question_id)
+    #data_handler.update_original_file2()
     return redirect('/')
 
 
 @app.route('/question/<question_id>/new-answer')
 def write_new_answer(question_id: int):
-    question_data = data_handler.get_question(question_id)
-    return render_template('post_answer.html',
+    question_data = data_handler.get_csv_data(id=question_id, key='id')
+    question_data = data_handler.get_timeform_from_stamp(question_data, False)
+    return render_template('new_answer.html',
                            question=question_data,
                            question_id=question_id,
                            page_title=question_data['title']
                            )
 
 
-@app.route('/question/<question_id>/new-answer', methods=['POST'])
+@app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
 def post_new_answer(question_id):
-    return redirect('/')
+    data_handler.add_message_to_file(request.form, 'data/answer.csv', question_id)
+    return redirect('/question/'+question_id)
 
 
 if __name__ == '__main__':
