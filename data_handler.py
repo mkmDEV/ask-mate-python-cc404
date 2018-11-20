c0b2812e43e5273407b8c01d19d35482ec6b08a4
csepdo <arodzeta@gmail.com>
2018-11-20 01:01:37 +0100

    Created queries, modified templates to get data from queries.

--- data_handler.py
+++ data_handler.py
@@ -6,20 +6,70 @@ import os
 from operator import itemgetter
 
 
-def get_csv_data(file='data/question.csv', id=None, key=None, isquestions=True):
-    all_data = []
-    with open(file, encoding='utf-8') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            single_data = dict(row)
-            if isquestions:
-                if id is not None and id == single_data[key]:
-                    return single_data
-                all_data.append(single_data)
-            else:
-                if id is not None and id == single_data[key]:
-                    all_data.append(single_data)
-        return all_data
+import database_common
+
+
+@database_common.connection_handler
+def show_questions(cursor):
+    cursor.execute("""SELECT * FROM question;""")
+    question_all = cursor.fetchall()
+    return question_all
+
+
+@database_common.connection_handler
+def show_question_by_id(cursor, question_id):
+    cursor.execute("""SELECT * FROM question
+                    WHERE id=%(question_id)s;""",
+                   {'question_id': question_id})
+    question_by_id = cursor.fetchall()
+    return question_by_id
+
+
+@database_common.connection_handler
+def get_question_title(cursor, question_id):
+    cursor.execute("""SELECT title FROM question
+                    WHERE id=%(question_id)s""",
+                   {'question_id': question_id})
+    question_title = cursor.fetchall()
+    for item in question_title:
+        title = item['title']
+    return title
+
+
+@database_common.connection_handler
+def show_answers(cursor, question_id):
+    cursor.execute("""SELECT * FROM answer
+                    WHERE question_id=%(question_id)s;""",
+                   {'question_id': question_id})
+    answer_all = cursor.fetchall()
+    return answer_all
+
+
+###################
+@database_common.connection_handler
+def add_question(cursor, new_question):
+    cursor.execute("""INSERT INTO question (submission_time, view_number, title, message, image) 
+                      VALUES ('<new_question>');""")
+
+
+###################
+@database_common.connection_handler
+def add_message(cursor, new_answer):
+    cursor.execute("""INSERT INTO quiestion
+                      VALUES '<new_answer>';""")
+
+
+@database_common.connection_handler
+def remove_question_from_database(cursor, question_id):
+    cursor.execute("""DELETE FROM question WHERE id=%(question_id)s;
+                    DELETE FROM answer WHERE question_id=%(question_id)s;""",
+                   {'question_id': question_id})
+
+
+@database_common.connection_handler
+def remove_answer_from_database(cursor, answer_id):
+    cursor.execute("""DELETE FROM answer WHERE id=%(answer_id)s;""",
+                   {'answer_id': answer_id})
 
 
 def get_time_form_from_stamp(table, is_table=True):
@@ -37,38 +87,38 @@ def get_unix_time():
     return str(calendar.timegm(time.gmtime()))
 
 
-def sorter(table, keyvalue, is_reverse=True):
-    return sorted(table, key=itemgetter(keyvalue), reverse=is_reverse)
+"""def sorter(table, keyvalue, is_reverse=True):
+    return sorted(table, key=itemgetter(keyvalue), reverse=is_reverse)"""
 
 
-def get_next_id(file):
+"""def get_next_id(file):
     existing_data = get_csv_data(file)
     if len(existing_data) == 0:
         return '1'
-    return str(int(existing_data[-1]['id']) + 1)
+    return str(int(existing_data[-1]['id']) + 1)"""
 
 
-def add_message_to_file(request, file='data/question.csv', q_id='0'):
+"""def add_message_to_file(request, file='data/question.csv', q_id='0'):
     if file == 'data/question.csv':
         fields = [get_next_id('data/question.csv'), get_unix_time(), '0', '0', request['title'], request['message']]
     else:
         fields = [get_next_id('data/answer.csv'), get_unix_time(), '0', q_id, request['message']]
     with open(file, 'a') as f:
         writer = csv.writer(f)
-        writer.writerow(fields)
+        writer.writerow(fields)"""
 
 
-def create_updated_file(id, file, col):
+"""def create_updated_file(id, file, col):
     with open(file, 'r') as f:
         with open('data/cache.csv', 'w') as f1:
             for line in f:
                 if line[col] != id:
-                    f1.write(line)
+                    f1.write(line)"""
 
 
-def update_original_file(file):
+"""def update_original_file(file):
     with open('data/cache.csv', 'r') as f:
         with open(file, 'w') as f1:
             for line in f:
                 f1.write(line)
-    os.remove('data/cache.csv')
+    os.remove('data/cache.csv')"""
--- server.py
+++ server.py
@@ -7,24 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def home():
-    questions = data_handler.get_csv_data()
-    questions = data_handler.sorter(questions, 'submission_time')
-    questions = data_handler.get_time_form_from_stamp(questions)
+    questions = data_handler.show_questions()
+    #questions = data_handler.sorter(questions, 'submission_time')
+    #questions = data_handler.get_time_form_from_stamp(questions)
     return render_template('list.html',
                            questions=questions,
                            page_title='Welcome to AskMate!')
 
 
 @app.route('/question/<question_id>')
-def display_question(question_id: int):
-    question_data = data_handler.get_csv_data(id=question_id, key='id')
-    question_data = data_handler.get_time_form_from_stamp(question_data, False)
-    answers = data_handler.get_csv_data('data/answer.csv', question_id, 'question_id', False)
-    answers = data_handler.sorter(answers, 'submission_time')
-    answers = data_handler.get_time_form_from_stamp(answers)
+def display_question(question_id):
+    question_data = data_handler.show_question_by_id(question_id)
+    print(question_data)
+    answers = data_handler.show_answers(question_id)
+    title = data_handler.get_question_title(question_id)
     return render_template('single_question.html',
                            question=question_data,
-                           page_title=question_data['title'],
+                           page_title=title,
                            answers=answers,
                            )
 
@@ -36,40 +35,39 @@ def write_new_question():
 
 @app.route('/new-question', methods=['POST'])
 def post_new_question():
-    data_handler.add_message_to_file(request.form)
+    new_question = dict(request.form)
+    data_handler.add_question(new_question)
     return redirect('/')
 
 
 @app.route('/question/<question_id>/delete')
 def delete_question(question_id: int):
-    data_handler.create_updated_file(question_id, 'data/question.csv', 0)
-    data_handler.update_original_file('data/question.csv')
-    #data_handler.create_updated_file(question_id, 'data/answer.csv', 3)
-    #data_handler.update_original_file('data/answer.csv')
+    data_handler.remove_question(question_id)
     return redirect('/')
 
 
 @app.route('/question/<question_id>/new-answer')
-def write_new_answer(question_id: int):
-    question_data = data_handler.get_csv_data(id=question_id, key='id')
+def write_new_answer(question_id):
+    question_data = data_handler.add_message(question_id)
     question_data = data_handler.get_time_form_from_stamp(question_data, False)
+    title = data_handler.get_question_title(question_id)
     return render_template('new_answer.html',
                            question=question_data,
                            question_id=question_id,
-                           page_title=question_data['title']
+                           page_title=title
                            )
 
 
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
 def post_new_answer(question_id):
-    data_handler.add_message_to_file(request.form, 'data/answer.csv', question_id)
-    return redirect('/question/'+question_id)
+    new_answer = dict(request.form)
+    data_handler.add_question(question_id, new_answer)
+    return redirect('/question/' + question_id)
 
 
 @app.route('/question/<question_id>/answer/<answer_id>')
 def delete_answer(question_id, answer_id: int):
-    data_handler.create_updated_file(answer_id, 'data/answer.csv', 0)
-    data_handler.update_original_file('data/answer.csv')
+    data_handler.remove_answer_from_database(answer_id)
     return redirect('/question/' + question_id)
 
 
