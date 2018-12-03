702173b9d6ba89dd177151ba221e00b84c4d01b7
csepdo <arodzeta@gmail.com>
2018-12-03 14:08:00 +0100

    requirements.txt updated

--- password_verfication.py
+++ password_verfication.py
@@ -2,7 +2,6 @@ import bcrypt
 
 
 def hash_password(plain_text_password):
-    # By using bcrypt, the salt is saved into the hash itself
     hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
     return hashed_bytes.decode('utf-8')
 
