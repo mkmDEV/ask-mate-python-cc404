5e6410b3018633114003832c0e729ee8eca23d1e
csepdo <arodzeta@gmail.com>
2018-12-04 11:36:16 +0100

    Initial commit.

--- password_verfication.py
+++ password_verfication.py
@@ -9,3 +9,7 @@ def hash_password(plain_text_password):
 def verify_password(plain_text_password, hashed_password):
     hashed_bytes_password = hashed_password.encode('utf-8')
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+def registration():
+    pass
