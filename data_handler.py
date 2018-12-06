cbdf60d0c5faa64b066f6d9d0c00542fea88521c
csepdo <arodzeta@gmail.com>
2018-12-06 14:25:45 +0100

    CORRECTED: List all users query and server done.

--- data_handler.py
+++ data_handler.py
@@ -134,8 +134,12 @@ def list_all_users(cursor):
     cursor.execute("""SELECT DISTINCT "user".user_name, "user".user_email, "user".reg_time,
                     (SELECT COUNT(question.id) FROM question WHERE question.username="user".user_name) as question,
                     (SELECT COUNT(answer.id) FROM answer WHERE answer.username="user".user_name) as answer,
-                    (SELECT COUNT("comment".id) FROM "comment" WHERE "comment".username="user".user_name) as "comment",
-                    FROM "user", question, answer, "comment"
+                    (SELECT COUNT("comment".id) FROM "comment" WHERE "comment".username="user".user_name) as "comment"
+                    FROM "user"
+                    LEFT JOIN question ON "user".user_name=question.username
+                    LEFT JOIN answer ON "user".user_name=answer.username
+                    LEFT JOIN "comment" ON "user".user_name="comment".username
                     WHERE "user".user_name=question.username AND "user".user_name=answer.username AND "user".user_name="comment".username
+                    OR (question.username IS NULL)
                     GROUP BY "user".user_name, question.id, answer.id, "comment".id""")
     return cursor.fetchall()
